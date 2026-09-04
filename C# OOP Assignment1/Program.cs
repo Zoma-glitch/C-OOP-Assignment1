0e611ac666a4fc79101ac920fe02575e803e2423
Zoma-glitch <hazem.safwat.55@gmail.com>
2026-09-04 20:17:20 +0300

    Question01

--- C# OOP Assignment1/Program.cs
+++ C# OOP Assignment1/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+            //1- What happens when a Customer variable is copied into another variable and one variable modifies the object
+            //address1.City=Cairo
+            //address2.City = Giza
+            //2- What happens when a Customer variable is copied into another variable and one variable modifies the object
+            //customer1.Name = Mohamed
+            //customer2.Name = Mohamed
+            #endregion
         }
     }
 }
